bf026ee569ec7ac2659cdca51c95936d8fc15fb4
caveman0612 <75807458+caveman0612@users.noreply.github.com>
2021-03-16 12:06:18 -0700

    added nav bar and connected the resume app to the stock app. i hit an issue with the z index which lead to a fucked up stack issue on the resume app, but i fixed it somehow, but im not sure how.

--- mysite/resume/urls.py
+++ mysite/resume/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
     path('pdf_resume/', views.pdf_resume, name='pdf_resume'),
-    path('projects', views.projects, name='projects'),
+    path('projects/', views.projects, name='projects'),
 ]
