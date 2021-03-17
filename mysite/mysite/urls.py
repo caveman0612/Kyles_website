418be5781934aeffb17d75b2ca3b8651c4b4bb5d
caveman0612 <75807458+caveman0612@users.noreply.github.com>
2021-03-16 17:04:10 -0700

    save point for css tinker

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('resume.urls')),
+    path('', include('resume.urls')),
     path('stocks/', include('stocks.urls')),
     path('security/', include('security.urls'))
 ]
