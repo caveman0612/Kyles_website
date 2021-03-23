2caf2d5ae934d1e7563427c0fe4d5aca48270c47
caveman0612 <75807458+caveman0612@users.noreply.github.com>
2021-03-23 12:18:21 -0700

    fixed projects page

--- mysite/stocks/views.py
+++ mysite/stocks/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import NameForm
 
 def companies (response):
     return render(response, "stocks/companies.html")
 
 def start_page(response):
-    return render(response, "stocks/start_page.html")
+    #make instance of form
+    if response.method == "POST":
+        form = NameForm(response.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/stocks/thanks/')
+    else:
+        form = NameForm()
+
+    return render(response, "stocks/start_page.html", {'form': form})
+    # return render(response, "stocks/start_page.html")
 
 def company_name(response):
     return render(response, "stocks/company_name.html")
