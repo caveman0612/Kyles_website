c50df73374c2db70fc9f86c3b1d2c7a34ee2700c
caveman0612 <kyle.stinar@hotmail.com>
2021-03-29 11:42:42 -0700

    added database with company and and list

--- mysite/stocks/views.py
+++ mysite/stocks/views.py
@@ -1,20 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from .forms import NameForm
+from.models import ListOfCompanies, Company
 
 def companies (response):
     return render(response, "stocks/companies.html")
 
 def start_page(response):
-    #make instance of form
-    if response.method == "POST":
-        form = NameForm(response.POST)
-        if form.is_valid():
-            return HttpResponseRedirect('/stocks/thanks/')
-    else:
-        form = NameForm()
+    ls = ListOfCompanies.objects.get(id=1)
+    item = ls.company_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br><p>%s</p>" %(ls.name, str(item.text)))
 
-    return render(response, "stocks/start_page.html", {'form': form})
+    # return render(response, "stocks/start_page.html", {'form': form})
     # return render(response, "stocks/start_page.html")
 
 def company_name(response):
