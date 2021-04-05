5353374588ffc57884d1bb22ce2129355005a590
caveman0612 <kyle.stinar@hotmail.com>
2021-04-05 15:22:52 -0700

    added different lists

--- mysite/stocks/urls.py
+++ mysite/stocks/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.start_page, name='start_page'),
-    # path("<int:id>", views.companies, name="companies"),
+    path("short_list/", views.short_list, name="short list"),
     path('companies/', views.companies, name='companies'),
     path('company_name/', views.company_name, name='company_name')
+    # path('stocks/short_list/', views.short_list, name="short list")
 ]
--- mysite/stocks/views.py
+++ mysite/stocks/views.py
@@ -4,14 +4,18 @@ from .forms import NameForm
 from.models import ListOfCompanies, Company
 
 def companies (response):
-    ls = ListOfCompanies.objects.get(id=1)
-    item = ls.company_set.get(id=1)
-    # return render(response, "stocks/start_page.html", {"ls": ls})
-    return render(response, "stocks/companies.html", {"ls": ls, "item": item})
-
-def start_page(response):
     # ls = ListOfCompanies.objects.get(id=1)
     # item = ls.company_set.get(id=1)
+    # return render(response, "stocks/start_page.html", {"ls": ls})
+    return render(response, "stocks/companies.html")
+
+def short_list(response):
+    ls = ListOfCompanies.objects.get(id=1)
+    item = ls.company_set.get(id=1)
+    return render(response, "stocks/short_list.html", {"ls":ls, "item": item})
+
+def start_page(response):
+
     return render(response, "stocks/start_page.html")
 
     # return HttpResponse("<h1>%s</h1><br><p>%s</p>" %(ls.name, str(item.text)))
